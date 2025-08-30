538f834dcfc3fc28f3491e001d849dd7ab742f11
Willem Wyndham <willem@ahalabs.dev>
2025-08-30 14:19:45 -0400

    feat: admin_v2

--- admin_v2/src/lib.rs
+++ admin_v2/src/lib.rs
@@ -1,8 +1,10 @@
 #![no_std]
-
-use soroban_sdk::{Address, Env, contract, contractimpl};
+use soroban_sdk::{Address, Env, String, contract, contractimpl};
 
 use admin_sep::{Administratable, Upgradable};
+
+use crate::auth::Funcs;
+mod auth;
 
 #[contract]
 pub struct Contract;
@@ -13,8 +15,15 @@ impl Contract {
         Self::set_admin(env, admin);
     }
 
-    pub fn new_method(env: &Env) {
+    pub fn new_method(env: &Env) -> String {
+        let address: Address = env.storage().instance().get(&Funcs::NewMethod).unwrap();
+        address.require_auth();
+        String::from_str(env, "hello world")
+    }
+
+    pub fn add_role(env: &Env, role: &Address) {
         Self::require_admin(env);
+        env.storage().instance().set(&Funcs::NewMethod, role);
     }
 }
 
